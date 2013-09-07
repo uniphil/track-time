d9e579da67e363f061a306c50253b80d7b64fef4
Phil Schleihauf <uniphil@gmail.com>
2013-09-07 19:05:48 -0400

    rest sketch

--- tracker.py
+++ tracker.py
@@ -27,24 +27,20 @@ mime_map = {
 
 
 def acceptable(**type_render):
-    print("setting up acceptable with", ", ".join(type_render))
     def wrapper(func):
-        print("wrapping", func)
         @wraps(func)
         def responder(*args, **kwargs):
-            print("responding...")
-            data = func(*args, **kwargs)
             accepted = (m for m, s in mime_map.items() if s in type_render)
             wants = request.accept_mimetypes.best_match(accepted)
             if wants is None:
                 abort(406)  # unacceptable type
-            data = func(*args, **kwargs)
-            return type_render[mime_map[wants]](data)
+            stuff = func(*args, **kwargs)
+            return type_render[mime_map[wants]](stuff)
         return responder
     return wrapper
 
 
-jinja2 = lambda template: lambda data: render_template(template, data=data)
+jinja2 = lambda template: lambda stuff: render_template(template, stuff=stuff)
 
 
 @app.route('/')
@@ -57,25 +53,42 @@ def hello():
 @acceptable(html=jinja2('task_index.html'), json=jsonify)
 def tasks_index():
     if request.method == 'POST':
-
+        raise NotImplementedError('lalala')
+    return data.get_tasks()
 
 
 @app.route('/tasks/<id>', methods=['GET', 'PUT', 'PATCH', 'DELETE'])
 @acceptable(html=jinja2('task.html'), json=jsonify)
 def task(id):
-    return {}
+    if request.method == 'GET':
+        return data.get_task(id)
+    elif request.method == 'PUT':
+        raise NotImplementedError('lalala')
+    elif request.method == 'PATCH':
+        raise NotImplementedError('lalala')
+    elif request.method == 'DELETE':
+        raise NotImplementedError('lalala')
 
 
 @app.route('/projects/', methods=['GET', 'POST'])
 @acceptable(html=jinja2('project_index.html'), json=jsonify)
 def projects():
-    return 'projects'
+    if request.method == 'POST':
+        raise NotImplementedError('lalala')
+    return data.get_some_project_stuff()
 
 
 @app.route('/projects/<id>', methods=['GET', 'PUT', 'PATCH', 'DELETE'])
 @acceptable(html=jinja2('project.html'), json=jsonify)
 def project(id):
-    return {}
+    if request.method == 'GET':
+        return data.projectblah(id)
+    elif request.method == 'PUT':
+        raise NotImplementedError('lalala')
+    elif request.method == 'PATCH':
+        raise NotImplementedError('lalala')
+    elif request.method == 'DELETE':
+        raise NotImplementedError('lalala')
 
 
 if __name__ == '__main__':
