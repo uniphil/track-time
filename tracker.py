41b802988d251e1781f4e32fa9032f4a1dc26de9
Phil Schleihauf <uniphil@gmail.com>
2013-09-09 11:24:49 -0400

    rest methods outline and data api mapping

--- tracker.py
+++ tracker.py
@@ -54,20 +54,38 @@ def hello():
 def tasks_index():
     if request.method == 'POST':
         raise NotImplementedError('lalala')
+        # validate input
+        stuff = {}
+        new_task = data.Task(**stuff)
+        new_task.save()
+        # return 303 see other
     return data.get_tasks()
 
 
 @app.route('/tasks/<id>', methods=['GET', 'PUT', 'PATCH', 'DELETE'])
 @acceptable(html=jinja2('task.html'), json=jsonify)
 def task(id):
+    this_task = data.get_task(id)
     if request.method == 'GET':
-        return data.get_task(id)
+        return this_task
     elif request.method == 'PUT':
         raise NotImplementedError('lalala')
+        # validate task
+        updated = {}
+        this_task.update(updated)
+        this_task.save()
+        return this_task
     elif request.method == 'PATCH':
         raise NotImplementedError('lalala')
+        # validate update
+        update = {}
+        this_task.update(update)
+        this_task.save()
+        return this_task
     elif request.method == 'DELETE':
         raise NotImplementedError('lalala')
+        this_task.remove()
+        # return 204 no content
 
 
 @app.route('/projects/', methods=['GET', 'POST'])
@@ -75,20 +93,38 @@ def task(id):
 def projects():
     if request.method == 'POST':
         raise NotImplementedError('lalala')
+        # validate input
+        stuff = {}
+        new_project = data.Project(**stuff)
+        new_project.save()
+        # Return 303 see other
     return data.get_some_project_stuff()
 
 
 @app.route('/projects/<id>', methods=['GET', 'PUT', 'PATCH', 'DELETE'])
 @acceptable(html=jinja2('project.html'), json=jsonify)
 def project(id):
+    this_project = data.get_project(id)
     if request.method == 'GET':
-        return data.projectblah(id)
+        return this_project
     elif request.method == 'PUT':
         raise NotImplementedError('lalala')
+        # validate task
+        updated = {}
+        this_project.update(updated)
+        this_project.save()
+        return this_project
     elif request.method == 'PATCH':
         raise NotImplementedError('lalala')
+        # validate update
+        update = {}
+        this_project.update(update)
+        this_project.save()
+        return this_project
     elif request.method == 'DELETE':
         raise NotImplementedError('lalala')
+        this_task.remove()
+        # return 204 no content
 
 
 if __name__ == '__main__':
